b4fe7b14bafc342c7f7d8d5c13532e4a361b8ef4
Uday <uday-jangra@talink.io>
2024-10-09 20:47:30 -0400

    feat: fixed rerender issue added formik on last form

--- apps/web/src/pages/VerifyEmail.tsx
+++ apps/web/src/pages/VerifyEmail.tsx
@@ -1,52 +1,62 @@
-import { useContext, useEffect, useState } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { memo, useCallback, useContext, useEffect, useState } from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import { UserContext } from '../context/authContext'
 import '../css/loginRegister.css'
-import { Navigate, useSearchParams } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import Loader from '../components/Spinner'
 
 function VerifyEmail() {
   const [searchParams] = useSearchParams()
   const [loading, setLoading] = useState(false)
-  const context = useContext(UserContext)
-  const handleVerification = async () => {
+  const [message, setMessage] = useState('')
+  const { verifyEmailCode } = useContext(UserContext)
+  const handleVerification = useCallback(async () => {
     const code = searchParams.get('oobCode')
     const mode = searchParams.get('mode')
     setLoading(true)
     if (!mode) {
-      setLoading(false)
-    }
-
-    if (mode !== 'verifyEmail') {
+      setMessage('Invalid verification link')
       setLoading(false)
     }
 
     if (!code) {
+      setMessage('Invalid verification link')
       setLoading(false)
-    } else {
+    }
+
+    if (code && mode === 'verifyEmail') {
       try {
-        await context.verifyEmailCode(code)
+        await verifyEmailCode(code)
+        setMessage('Email verified successfully')
+        setLoading(false)
       } catch (error) {
         console.log(error)
+        setMessage('Error verifying email')
         setLoading(false)
       }
     }
-  }
+  }, [searchParams, verifyEmailCode])
 
   useEffect(() => {
+    console.log('useEffect')
     handleVerification()
-  }, [])
+  }, [handleVerification])
 
   return (
     <Container className="container-full-right">
       <Row className="justify-content-md-center">
         <Col className="form-container">
           {loading && <Loader />}
-          {!loading && <>{<Navigate to="/login" />}</>}
+          {!loading && (
+            <>
+              <h1>{message}</h1>
+              <Link to="/login">Login</Link>{' '}
+            </>
+          )}
         </Col>
       </Row>
     </Container>
   )
 }
 
-export default VerifyEmail
+export default memo(VerifyEmail)
